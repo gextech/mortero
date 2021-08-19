353a0fa95444697381b56d2f7aa0d6d1386787d4
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-08-19 11:30:35 -0500

    fix: keep remote imports untouched

--- src/source.js
+++ src/source.js
@@ -180,6 +180,7 @@ class Source {
       }
     } else {
       text = text.replace(RE_IMPORT, (_, $1, $2, $3, $4, $5) => {
+        if (/^https?:\/\//.test($5)) return _;
         if (_.length > 250 || _.includes('data:')) return _;
         if (_.indexOf('url(') === 0) {
           return tpl.extension === 'js' ? _ : `url(${$1}#!@@locate<${$2}>${$1}`;
